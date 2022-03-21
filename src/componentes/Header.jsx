77a7f50ec0cb431fff25152aa3bf61f8cf4402eb
Raul Balanzar Mosqueda <desarrolloweb.balanzar@gmail.com>
2022-03-20 23:51:44 -0600

    Header
    Cambios en el nombre del Header

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -5,8 +5,8 @@ const Header = () => {
 		<>
 			{" "}
 			<h1 className=" font-black text-5xl text-center md:w-2/3 mx-auto">
-				Seguimiento pacientes{" "}
-				<span className=" text-indigo-600">veterinaria</span>{" "}
+				Seguimiento Pacientes{" "}
+				<span className=" text-indigo-600">Veterinaria</span>{" "}
 			</h1>
 		</>
 	);
